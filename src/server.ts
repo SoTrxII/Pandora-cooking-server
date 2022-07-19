8f4cdc211d6a6e8b5f4f485d46ad4065e31459cf
SoTrx <-->
2022-07-19 15:28:45 +0200

    Enabling the server to parse JSON

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import "reflect-metadata";
 import "./controllers/records.controller";
-
+import { json } from "express";
 import { container } from "./inversify.config";
 import { InversifyExpressServer } from "inversify-express-utils";
 import { ILogger } from "./pkg/logger/logger-api";
 import { TYPES } from "./types";
 const server = new InversifyExpressServer(container);
 const app = server.build();
+
+// Allow the server to parse JSON body
+server.setConfig((app) => app.use(json()));
 const PORT = 3004;
 app.listen(PORT, () => {
   const logger = container.get<ILogger>(TYPES.Logger);
