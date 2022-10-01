522c85a0294f46b905ea86019694ec2c4ae125c2
SoTrx <-->
2022-10-01 13:13:18 +0200

    Configuring express/json to parse dapr json+cloudevents

--- src/server.ts
+++ src/server.ts
@@ -7,10 +7,13 @@ import { ILogger } from "./pkg/logger/logger-api";
 import { TYPES } from "./types";
 
 const server = new InversifyExpressServer(container);
-const app = server.build();
 
 // Allow the server to parse JSON body
-server.setConfig((app) => app.use(json()));
+server.setConfig((app) => {
+  app.use(json({ type: "application/*+json" }));
+});
+
+const app = server.build();
 const PORT = 3004;
 app.listen(PORT, () => {
   const logger = container.get<ILogger>(TYPES.Logger);
